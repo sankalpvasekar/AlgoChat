04a47e37a872e5771e9eedd2a3cd1b2fb4908c68
sankalpvasekar <sankalpvasekar@gmail.com>
2026-03-12 19:19:25 +0530

    refactor: simplify DATABASES config in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,8 +89,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600
+        default=os.environ.get("DATABASE_URL")
     )
 }
 
